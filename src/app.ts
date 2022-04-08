60c0e18bf5cc3155585eb82756afd82605ab3973
msalafia <msalafia@seamgroup.com>
2022-04-07 21:11:14 -0300

    Agregamos GET basico

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.info("Iniciamos con app");
+import express from 'express';
+
+const PORT = 5005;
+const server = express();
+
+server.get("/", (req, res) => {
+    res.send("<html><body><h1>Hola Mundo</h1></body></html>");
+});
+
+server.listen(PORT, () => {
+    console.info(`Iniciando servidor en puerto ${PORT}`)
+});
