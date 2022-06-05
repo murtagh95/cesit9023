32da09311388bb31c4571d364cd1eb5f86eb7e13
Lautaro <72414443+decimalautaro@users.noreply.github.com>
2022-06-05 17:05:02 -0300

    IES-118 cambio tipo de dni de number a string (#5)
    - cambio tipo de dni de number a string

--- src/models/Alumno.ts
+++ src/models/Alumno.ts
@@ -5,9 +5,9 @@ import { IModeloBase, modeloBase } from "./ModeloBase";
 export interface IAlumno extends IModeloBase{
     nombre: string;
     apellido: string;
-    dni: number;
-    domicilio: string
-    fechaNacimiento: Date
+    dni: string;
+    domicilio: string;
+    fechaNacimiento: Date;
 
 }
 
@@ -15,7 +15,7 @@ const alumnoSchema = new Schema<IAlumno>({
     ...modeloBase,
     nombre: { type: String, required: true },
     apellido: { type: String, required: true },
-    dni: { type: Number, required: true },
+    dni: { type: String, trim: true, unique: true,required: true },
     domicilio: { type: String, required: true },
     fechaNacimiento: { type: Date, required: true },
 });
