c2191f16c843f53b9607ec0b7baea0beeb4c049a
Murtagh95 <nec.catalano@gmail.com>
2022-06-05 00:24:44 -0300

    feat: creacion de modelo base para los diferentes Schemas

---
+++ src/models/ModeloBase.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose';
+
+
+export interface IModeloBase {
+    _id?: string;
+    fechaCreacion: Date;
+    fechaActualizacion: Date;
+    baja: boolean;
+
+}
+
+export const modeloBase = {
+    fechaCreacion: { type: Date, required: true, default: new Date },
+    fechaActualizacion: { type: Date, required: true, default: new Date },
+    baja: { type: Boolean, required: true, default: false }
+}
