e06b8c69a9a7a37da1eee6e4f16b9963c3bc38a0
Nicolas Conil <72760200+NicoC016@users.noreply.github.com>
2022-06-06 10:15:20 -0300

    IES-103 agregar materia basica (#11)
    * agregando materias
    
    * agregando materias
    
    * actualizacion de materias
    
    * agregacion y mejora de Materia
    
    * actualizacion y creacion de Materia
    
    * creacion y actualizacion de materia
    
    * actualizacion de Materia

---
+++ src/models/Materia.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose';
+import { IModeloBase, modeloBase } from './ModeloBase';
+
+export enum CondicionMateria {
+    Regular = "regular",
+    Promocion = "promocion",
+    Libre = "libre",
+    Recursa = "recursa",
+}
+export interface IMaterias extends IModeloBase {
+    _id?: string;
+    nombre: string;
+    profesor: string;
+    duracion: number;
+    condicionMateria:CondicionMateria
+}
+
+
+const MateriaSchema = new Schema<IMaterias>({
+    ...modeloBase,
+    nombre: { type: String, required: true },
+    profesor: {type:String, require: true},
+    duracion: { type: Number, required: true },
+    condicionMateria:{ type:String , enum:Object.values(CondicionMateria),required:true}
+});
+
+export const Materia = model<IMaterias>('materias', MateriaSchema);
