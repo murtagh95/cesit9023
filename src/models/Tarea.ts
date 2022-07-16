4b278f43dccd00f7ca2b037e9bdd2ef90a22c749
Hugo Pacini <hugo.pacini@halopowered.com>
2022-07-16 00:02:33 -0300

    Agrego schema

--- src/models/Tarea.ts
+++ src/models/Tarea.ts
@@ -5,12 +5,14 @@ export interface ITarea {
   nombre: string;
   descripcion?: string;
   finalizada?: boolean;
+  fechaLimite?: boolean;
 }
 
 const tareaSchema = new Schema<ITarea>({
   nombre: { type: String, required: true },
   descripcion: { type: String, required: false },
   finalizada: { type: Boolean, default: false },
+  fechaLimite: { type: Date, required: true },
 });
 
 export const Tarea = model<ITarea>('tareas', tareaSchema);
