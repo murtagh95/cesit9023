4da226cebd1d39f5b2fc74cd4646a8fa1e48cef2
nicolasCatalanoSoutherncode <nicolas.catalano@southerncode.us>
2022-11-01 18:37:56 -0300

    fix: se cambia variable de entorno

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const DATE_FORMAT = 'dd/MM/yyyy';
-export const back_end_url = process.env.BACKEND_URL || 'https://b451-191-81-181-82.sa.ngrok.io'
+export const back_end_url = process.env.BACKEND_URL || 'https://07a7-181-117-236-120.sa.ngrok.io/'
