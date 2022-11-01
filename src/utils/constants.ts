01d34d58a4f550c5eebdce682eea1bf391036878
nicolasCatalanoSoutherncode <nicolas.catalano@southerncode.us>
2022-11-01 18:39:57 -0300

    fix: url backend

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const DATE_FORMAT = 'dd/MM/yyyy';
-export const back_end_url = process.env.BACKEND_URL || 'https://07a7-181-117-236-120.sa.ngrok.io/'
+export const back_end_url = process.env.BACKEND_URL || 'https://07a7-181-117-236-120.sa.ngrok.io'
