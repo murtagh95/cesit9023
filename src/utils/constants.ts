6acefff5ea0db356ed422b403e8c0d0a18c8c666
Murtagh95 <nec.catalano@gmail.com>
2022-11-01 00:28:34 -0300

    feat(utils/constants): se saca la url del backend a una variable de entorno

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const DATE_FORMAT = 'dd/MM/yyyy';
-export const back_end_url = 'https://5ed3-191-81-160-178.sa.ngrok.io/'
+export const back_end_url = process.env.BACKEND_URL || 'localhost:5005'
